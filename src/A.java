be9f9805984d2bf18815368f76cfa133bf004822
Estudiante <Estudiante@FI454402-34.unal.loc>
2016-10-18 15:59:08 -0500

    Interface

---
+++ src/A.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public interface A {
+    public final int w = 10;
+    
+    public abstract void xx();
+    public abstract void yy();
+}
